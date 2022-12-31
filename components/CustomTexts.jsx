bd65c1edf5ba08e0dd2fb5079e7085f3e074284a
Kevin Everrett <everrett.developer@gmail.com>
2022-12-31 14:18:31 -0500

    About section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,19 @@
-'use client';
+import { motion } from 'framer-motion';
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import styles from '../styles';
+import { fadeIn, textContainer, textVariant2 } from '../utils/motion';
+
+export const TypingText = ({ title, textStyles }) => (
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[20px] text-secondary-white ${textStyles}`}
+  >
+    {Array.from(title).map((letter, index) => (
+      <motion.span variants={textVariant2} key={index}>
+        {letter === '' ? '\u00a0' : letter}
+      </motion.span>
+    ))}
+  </motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
-);
+export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>;
